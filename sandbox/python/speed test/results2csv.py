5a29c491eaa11511e2057f9aa12e5414332dc05c
atabulog <austin.tabulog@gmail.com>
2022-01-02 22:31:28 -0600

    added script to convert all available data to a .csv file

--- sandbox/python/speed test/results2csv.py
+++ sandbox/python/speed test/results2csv.py
@@ -7,15 +7,20 @@ email: austin.tabulog@gmail.com
 This file is subject to the MIT license copyright notice.
 =========================================================
 
-Script to grab all results from networkData table in db.
+Script to create csv file for all results in db table.
 """
 
 #imports
 from mysql.connector.errors import DataError
 import dbIntfObj
+import csv
 
 if __name__ == "__main__":
     #create connection to database
     dbTable = dbIntfObj.networkDataTable()    
     dbTable.fetch_allData()
-    [print(x) for x in dbTable.results]
+    
+    with open('C:\\Users\\Austi\\Desktop\\networkData.csv', "w+") as f:
+        writer = csv.writer(f)
+        [writer.writerow(row) for row in dbTable.results]
+    
